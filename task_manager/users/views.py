9af7a75690cffc12b61cc1db5cf108cde15ec027
mistermikebell <jokerbell7@gmail.com>
2021-03-31 11:19:00 +0300

    step 3 Authentication v2

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,18 +1,19 @@
-from django.views import generic
-from task_manager.users.forms import SignUpForm
-from django.contrib.auth.models import User
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.contrib.auth.views import redirect_to_login
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views import generic
+from django.urls import reverse_lazy
+from django.utils.translation import gettext
+from task_manager.users.forms import SignUpForm
 
 
 class RegisterUser(SuccessMessageMixin, generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('home')
-    success_message = 'You have been signed up!'
+    success_message = gettext('You have been signed up!')
 
 
 class UsersList(LoginRequiredMixin, generic.ListView):
@@ -22,7 +23,8 @@ class UsersList(LoginRequiredMixin, generic.ListView):
 
     def handle_no_permission(self):
         if not self.request.user.is_authenticated:
-            messages.error(self.request, 'You do not have access to this page')
+            messages.error(self.request,
+                           gettext('You do not have access to this page'))
         return redirect_to_login(self.request.get_full_path(), 'login/', '')
 
 
@@ -32,8 +34,8 @@ class UpdateUser(LoginRequiredMixin, SuccessMessageMixin, generic.UpdateView):
     fields = ['username', 'email', 'password']
     login_url = 'login/'
     redirect_field_name = ''
-    permission_denied_message = 'You do not have access to this page'
-    success_message = 'Your profile has been updated'
+    permission_denied_message = gettext('You do not have access to this page')
+    success_message = gettext('Your profile has been updated')
 
     def get_success_url(self):
         return ''
@@ -43,5 +45,5 @@ class DeleteUser(LoginRequiredMixin, SuccessMessageMixin, generic.DeleteView):
     model = User
     success_url = reverse_lazy('home')
     template_name = 'registration/delete.html'
-    permission_denied_message = 'You do not have access to this page'
-    success_message = 'Your profile has been deleted'
+    permission_denied_message = gettext('You do not have access to this page')
+    success_message = gettext('Your profile has been deleted')
