cd6fe7e7a80e4dd1902bb12d52476bd3e31d2c2e
mistermikebell <jokerbell7@gmail.com>
2021-05-09 19:57:01 +0300

    hide secret_key

--- task_manager/filters.py
+++ task_manager/filters.py
@@ -7,8 +7,9 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class MyTasksFilter(django_filters.FilterSet):
-    # status = django_filters.ModelMultipleChoiceFilter(queryset=Status.objects.all())
-    status = django_filters.AllValuesMultipleFilter(field_name='status')
+    status = django_filters.ModelMultipleChoiceFilter(queryset=Status.objects.all())
+    # status = django_filters.AllValuesMultipleFilter(field_name='status')
+
     class Meta:
         model = Task
         fields = ['status', 'labels']
