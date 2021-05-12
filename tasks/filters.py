e466c00ad215649cc1820c08d028c1bcca4178dc
mistermikebell <jokerbell7@gmail.com>
2021-05-12 20:51:59 +0300

    fix status filter

--- tasks/filters.py
+++ tasks/filters.py
@@ -6,18 +6,20 @@ from django.utils.translation import ugettext_lazy as _
 from labels.models import Label
 from django.contrib.auth.models import User
 from tasks.models import UserModel
+from statuses.models import Status
 
 
 class TasksFilter(django_filters.FilterSet):
     boolean_author = django_filters.BooleanFilter(
         field_name='author', label=_('Show only my tasks'),
         widget=forms.CheckboxInput, method='filter_author')
-    labels = django_filters.ModelChoiceFilter(queryset=Label.objects.all(), label=_('Label'))
-    executor = django_filters.ModelChoiceFilter(queryset=UserModel.objects.all(), label=_('Executor'))
+    labels = django_filters.ModelChoiceFilter(queryset=Label.objects.all())
+    executor = django_filters.ModelChoiceFilter(queryset=UserModel.objects.all())
+    status = django_filters.ModelChoiceFilter(queryset=Status.objects.all())
 
-    class Meta:
-        model = Task
-        fields = ['status', 'labels', 'executor']
+    # class Meta:
+    #     model = Task
+    #     fields = ['status', 'labels', 'executor']
 
     def filter_author(self, queryset, name, value):
         if value:
